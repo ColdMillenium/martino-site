51091221a32007f58efda6c68d59a88fed5e34b1
ColdMillenium <andrewgerribrowning@gmail.com>
2021-02-20 17:00:43 -0500

    added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        copyright c 2020 Martino Pizza Inc. All Rights Rerseved
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style scoped>
+
+</style>
