0ad5eaa229ee86c20e71c03b2a5f92b92e376f53
ColdMillenium <andrewgerribrowning@gmail.com>
2021-02-20 20:11:19 -0500

    created basic meal Food item card

--- src/components/food/FoodItem.vue
+++ src/components/food/FoodItem.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
+    <div class="container">
+       
+        <!-- {{ prices.length == 1 ? 'SINGLE' : "MULTIsdfad"}}   -->
+         
         <img :src="picUrl" :alt="'picture of ' + dishName + 'meal'">
-        {{ prices.length == 1 ? 'SINGLE' : "MULTIsdfad"}}   
-        <div v-for="(meat, index) in meats" :key="meat">
-          
-            {{meat}}
-            {{prices[index]}}
+        <div v-if="prices.length>1" class="meal-text">
+            <div><h2 class="dish-name">{{dishName}}</h2></div>
+            <div><p class= "description" >{{desc}}</p></div>
+            
+            <div class="price-options">
+                <div class="meatItem" v-for="(meat, index) in meats" :key="meat">
+                    <p>{{meat}}</p>
+                    <h3>{{prices[index]}}</h3>
+                </div>
+            </div>
         </div>
+        
+        
     
     </div>
 </template>
@@ -32,5 +42,44 @@ export default {
 }
 </script>
 <style scoped>
-
+    .container{
+      
+        background-color: lightgrey;
+        width: 20em;
+        
+    }
+    img{
+        text-align:left;
+        width: 100%;
+        height: 30%;
+    }
+    p,h1,h2,h3{
+        padding: 0;
+        margin:0;
+    }
+    .meal-text *{
+        margin-left:0;
+        display:flex;
+        flex-direction: column;
+        padding: 5px;
+        text-align: left;
+        
+    }
+    .dish-name, .description{
+        margin-bottom: 1em;
+    }
+    .description{
+        
+    }
+    .price-options{
+        display:flex;
+        flex-direction: row;
+        gap: 1em;
+    }
+    
+    .meatItem{
+        /* background-color: green; */
+        
+        margin: 0;
+    }
 </style>
