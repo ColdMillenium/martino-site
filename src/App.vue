a5027f101c1a634023b97a9e7e69b06a85d9e6b5
ColdMillenium <andrewgerribrowning@gmail.com>
2021-02-20 19:26:49 -0500

    finally got mulit prices to display from array on meal

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,16 @@
     <PizzaWingCombo size="x-large" lbs="2" price="38.49"/>
     <PizzaWingCombo size="family" lbs="2 (1/2)" price="41.79"/>
     <PizzaWingCombo size="party" lbs="3" price="54.05"/>
+    <FoodItem 
+      picUrl="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563"
+      dishName= "Test Dish"
+      desc=  "It's the best test dish you've ever had. There's nothing like it."
+      :meats="['shrimp', 'beef', 'tofu']"
+      :prices="[19.99, 25.55, 34.55]"
+    />
+    woah waoh
     <Footer/>
+    
   </div>
 </template>
 
@@ -18,6 +27,7 @@ import Home from './components/Home.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import PizzaWingCombo from './components/food/PizzaWingCombo.vue'
+import FoodItem from './components/food/FoodItem.vue'
 
 export default {
   name: 'App',
@@ -25,7 +35,8 @@ export default {
     Home,
     Header,
     Footer,
-    PizzaWingCombo
+    PizzaWingCombo,
+    FoodItem,
   }
 }
 </script>
---
+++ src/components/food/FoodItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <img :src="picUrl" :alt="'picture of ' + dishName + 'meal'">
+        {{ prices.length == 1 ? 'SINGLE' : "MULTIsdfad"}}   
+        <div v-for="(meat, index) in meats" :key="meat">
+          
+            {{meat}}
+            {{prices[index]}}
+        </div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name:"FoodItem",
+    props:{
+      picUrl: String,
+      dishName: String,
+      prices: Array,
+      meats: Array,
+      price: Number,
+      desc: String
+    },
+    data (){
+        return {
+            pussy: this.instance.prices.length,
+            chicken: 'beach'
+        }
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
